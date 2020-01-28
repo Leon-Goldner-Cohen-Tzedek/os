5800d62813a880a0fdaa756fa6813aa89eda39d4
Leon Goldner Cohen Tzedek <leong@pop-os.localdomain>
2020-01-27 19:17:31 -0800

    VIDEO DRIVER BEYBEE!

---
+++ kernel/drivers/ports.c
@@ -0,0 +1,30 @@
+//read a byte from the specified port
+unsigned char byte_in(unsigned short port) 
+{	
+	unsigned char result;
+	//"a=" (result) assignes contents of dx to the variable result
+	//"d" (port) maps the variable port to the register d
+	__asm__("in %%dx, %%al" : "=a" (result) : "d" (port));
+
+	return result;
+}
+
+void byte_out(unsigned short port, unsigned char data)
+{
+	__asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+
+unsigned short word_in(unsigned short port)
+{
+	unsigned  short result; 
+	__asm__("in %%dx, %%ax" :  "=a" (result) :  "d" (port));
+	return result;
+}
+
+void word_out(unsigned short port, unsigned short data)
+{
+	__asm__("out %%ax, %%dx" : : "a" (data), "d" (port));
+}
+
+
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,13 @@
+#include "drivers/screen.c"
+
 void main()
 {
-	char* video_memory = (char*) 0xb8000;
-	*video_memory = 'X';
+	clear_screen();
+	kprint_at("XXXXXXXXXXX", 7, 3);
+	kprint_at("This text spans multiple lines", 75, 10);
+	kprint_at("There is a \nline\nbreak", 0, 15);
+	kprint_at("There is another\nline\nbreak", 0, 20);
+	kprint_at("What happens when we run out of space?", 45, 24);
+	
 }
 
